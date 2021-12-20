20627649e3fa6a6fc2ef5409798e1b6f2c2d6366
lnhatlinh200 <lnhatlinh2000@gmail.com>
2021-12-20 19:07:53 +0700

    init project Interview Process Management

---
+++ ipm_system/src/test/java/fa/training/salemanagementsystem/SaleManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package fa.training.salemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaleManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
